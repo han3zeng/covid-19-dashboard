06c7ff1fe41edcdf31ccc5c395629eed4d57d266
Han <allen0346@gmail.com>
2021-08-01 10:33:18 +0800

    Adjust: fetch from different data sources from based on env

--- utils/spreadsheet.js
+++ utils/spreadsheet.js
@@ -1,13 +1,16 @@
 const https = require('https')
 const http = require('http')
-const TARGET_PROD = 'https://tpts-public.s3.ap-southeast-1.amazonaws.com/covid-19-dashboard/content-covid-19-dashboard.json'
-const TARGET_DEV = 'http://localhost:8080/content-covid-19-dashboard-v2'
+const URL_PROD = 'https://tpts-public.s3.ap-southeast-1.amazonaws.com/covid-19-dashboard/content-covid-19-dashboard-v2.json'
+const URL_DEV = 'http://localhost:8080/content-covid-19-dashboard-v2'
+
+const scheme = process.env.NODE_ENV === 'production' ? https : http
+const url = process.env.NODE_ENV === 'production' ? URL_PROD : URL_DEV
 
 async function getSpreadsheetData () {
   const promise = new Promise((resolve, reject) => {
-    http
+    scheme
       .get(
-        TARGET_DEV,
+        url,
         (res) => {
           const { statusCode } = res
           const contentType = res.headers['content-type']
